e7fb46aa39120543362e36a8ae34a95ad8c2b7da
damnnn <damianojongerius@gmail.com>
2022-07-01 16:14:13 +0200

    hey

---
+++ Assets/player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ik ben er");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
